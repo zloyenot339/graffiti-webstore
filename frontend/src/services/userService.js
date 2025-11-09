045b5b9d5b2bc41e8592b0028b46301a5ab9bba7
Артём Зимогляд <git-user@selectel.ru>
2025-11-09 18:37:48 +0200

    Update userService

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -24,7 +24,6 @@ api.interceptors.response.use(
 
     if (error.response.status === 401 && !originalRequest._retry) {
       if (isRefreshing) {
-        // если уже идёт обновление токена, ждем его
         return new Promise((resolve, reject) => {
           failedQueue.push({ resolve, reject });
         })
